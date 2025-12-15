f56ba1d565b92d3bbf4ac35bb098f36e974e5b17
Sergio Sánchez Plaza <sergiosanchezplaza8@gmail.com>
2025-12-15 18:56:53 +0100

    practica_arreglafa

---
+++ RA5/Tarea2/js/cookies.js
@@ -0,0 +1,38 @@
+function setCookie(name, value, days, path = '/') {
+
+    let expires = ""
+    if (days) {
+
+        const date = new Date()
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+        expires = "; expires=" + date.toUTCString()
+
+
+    }
+
+    document.cookie = name + "=" + (value || "") + expires + "; path=" + path;
+
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+
+    // Usamos un bucle for tradicional para permitir la salida inmediata con 'return'
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        
+        // CORRECCIÓN 1: Usar charAt(0)
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length); 
+        
+        // CORRECCIÓN 2: El 'return' sale de la función getCookie
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function deleteCookie(name, path='/'){
+
+    document.cookie = name + "=; Max-Age=-99999999; path=" + path;
+
+}
